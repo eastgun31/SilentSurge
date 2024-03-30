97b299009f4a50125ccdc0c8a12d188124d27537
kaffly <kgh010105@naver.com>
2024-03-31 01:54:12 +0900

    회로 성공여부
    회로 성공 여부 완성

---
+++ Assets/KGH/Scripts/PipeManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeManager : MonoBehaviour
+{
+    public GameObject PipesHoldert;
+    public GameObject[] Pipes;
+
+    [SerializeField]
+    int totalPipes = 0;
+    [SerializeField]
+    int correctPipes = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalPipes = PipesHoldert.transform.childCount;
+
+        Pipes = new GameObject[totalPipes];
+
+        for (int i = 0; i < Pipes.Length; i++)
+        {
+            Pipes[i] = PipesHoldert.transform.GetChild(i).gameObject;
+        }
+    }
+
+    public void CorrectMove()
+    {
+        correctPipes += 1;
+
+        Debug.Log("correct");
+
+        if (correctPipes == totalPipes)
+        {
+            Debug.Log("Win");
+        }
+    }
+
+    public void WrongMove()
+    {
+        correctPipes -= 1;
+    }
+}
--- Assets/KGH/Scripts/Pipes.cs
+++ Assets/KGH/Scripts/Pipes.cs
@@ -10,27 +10,73 @@ public class Pipes : MonoBehaviour, IPointerClickHandler
     float[] rotation = { 0, 90, 180, 270 };
     public float rotationAngle = 90f;
 
-    public float correctRotation;
+    public float[] correctRotation;
     [SerializeField]
     bool isPlaces = false;
 
+    int PossibleRots = 1;
+
+    PipeManager pipeManager;
+
+    private void Awake()
+    {
+        pipeManager = GameObject.Find("PipeManager").GetComponent<PipeManager>();
+    }
+
     void Start()
     {
+        PossibleRots = correctRotation.Length;
         int rand = Random.Range(0,rotation.Length);
         transform.eulerAngles = new Vector3(0, 0, rotation[rand]);
 
-        if (transform.eulerAngles.z == correctRotation)
-            isPlaces = true;
+        if (PossibleRots > 1)
+        {
+            if (transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1])
+            {
+                isPlaces = true;
+                pipeManager.CorrectMove();
+            }
+        }
+        else
+        {
+            if (transform.eulerAngles.z == correctRotation[0])
+            {
+                isPlaces = true;
+                pipeManager.CorrectMove();
+            }
+        }
     }
     public void OnPointerClick(PointerEventData eventData)
-    {        
+    {
         RectTransform rt = GetComponent<RectTransform>();
 
         rt.Rotate(new Vector3(0, 0, rotationAngle));
 
-        if(transform.eulerAngles.z == correctRotation && isPlaces == false)
-            isPlaces = true;
-        else if(isPlaces == true)
-            isPlaces = false;
+        if (PossibleRots > 1)
+        {
+            if (transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1] && isPlaces == false)
+            {
+                isPlaces = true;
+                pipeManager.CorrectMove();
+            }
+            else if (isPlaces == true)
+            {
+                isPlaces = false;
+                pipeManager.WrongMove();
+            }
+        }
+        else
+        {
+            if (transform.eulerAngles.z == correctRotation[0] && isPlaces == false)
+            {
+                isPlaces = true;
+                pipeManager.CorrectMove();
+            }
+            else if (isPlaces == true)
+            {
+                isPlaces = false;
+                pipeManager.WrongMove();
+            }
+        }
     }
 }
