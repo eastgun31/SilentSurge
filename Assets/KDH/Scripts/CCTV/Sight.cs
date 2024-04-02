216595370afaa2d491fa03cba9f85ff0743da756
lcal0824 <kdh9438@naver.com>
2024-04-03 00:06:54 +0900

    시야 스크립트 수정
    미완성입니다

--- Assets/KDH/Scripts/CCTV/Sight.cs
+++ Assets/KDH/Scripts/CCTV/Sight.cs
@@ -12,6 +12,10 @@ public class Sight : MonoBehaviour
     public LayerMask playerM;
 
     public Collider isColP;
+    public EnemyLevel eLv;
+
+    public float lvupT;
+    public bool lvupC;
 
     public CCTVMovement cctv;
     public Enemy enemy;
@@ -42,18 +46,35 @@ public class Sight : MonoBehaviour
             {
                 float disT = Vector3.Distance(transform.position, detectTarget.position);
                 foreach(Collider col in targets)
-                { 
-                    if (this.name == "CCTV")    // CCTV가 플레이어 감지 후 즉시 탐지단계 상승
+                {
                     {
+                        if (this.tag == "Enemy")    // 적이 플레이어 감지 후 조건에 부합할 경우 탐지단계 상승
+                        {
 
-                    }
-                    if (this.tag == "Enemy")    // 적이 플레이어 감지 후 조건에 부합할 경우 탐지단계 상승
-                    {
-
+                        }
+                        if (this.name == "CCTV")    // CCTV가 플레이어 감지 후 즉시 탐지단계 상승
+                        {
+                            if (this.eLv.LvStep == EnemyLevel.ELevel.level1 && lvupC == true)
+                            {
+                                this.eLv.LvStep = EnemyLevel.ELevel.level2;
+                                DetectLvUpDelay();
+                            }
+                            else if (this.eLv.LvStep == EnemyLevel.ELevel.level2 && lvupC == true)
+                            {
+                                this.eLv.LvStep = EnemyLevel.ELevel.level3;
+                                DetectLvUpDelay();
+                            }
+                        }
                     }
                 }
             }
         }
+    }
+
+    void DetectLvUpDelay()
+    {
+        lvupC = false;
+        lvupT += Time.deltaTime;
     }
 
     public Vector3 DirFromAngle(float angleDegrees, bool angleIsGlobal) // 범위 그려주기 위해 필요한 함수 (오일러 각을 3차원 벡터 값으로 변환)
