15b0f0a267181e0437ce4525336df089500472ae
samgirlsss <ngjin12@naver.com>
2024-04-04 12:46:10 +0900

    .

--- Assets/KDH/Scripts/CCTV/Sight.cs
+++ Assets/KDH/Scripts/CCTV/Sight.cs
@@ -81,35 +81,36 @@ public class Sight : MonoBehaviour
     void DetectTargets()
     {
         Collider[] targets = Physics.OverlapSphere(transform.position, radius, playerM);
-        Debug.Log("targets의 배열은 1");
         for (int i = 0; i < targets.Length; i++)
         {
             Transform detectTarget = targets[i].transform;
             Vector3 dirT = (detectTarget.position - transform.position).normalized;
-            Debug.Log(dirT);
             if (Vector3.Angle(transform.forward, dirT) < angle / 2)
             {
-                Debug.Log("범위 안 플레이어 인식");
                 float disT = Vector3.Distance(transform.position, detectTarget.position);
-                Debug.Log(disT);
-                foreach(Collider col in targets)
+                foreach (Collider col in targets)
                 {
                     if (!Physics.Raycast(transform.position, dirT, disT, etcM))
                     {
-                        Debug.Log("장애물이 없을 때 플레이어 발견");
                         detectTarget = visibleT;
+                        if (enemy.dLevel == (int)eLv.LvStep && this.tag=="Enemy")
+                        {
+                            eLv.LvStep = EnemyLevel.ELevel.level2;
+                        }
+                        if (enemy.dLevel == (int)eLv.LvStep && this.tag == "Enemy")
+                        {
+                            eLv.LvStep = EnemyLevel.ELevel.level3;
+                        }
                     }
                     if (this.tag == "Enemy")    // 적이 플레이어 감지 후 조건에 부합할 경우 탐지단계 상승
                     {
-                        if (eLv.LvStep == EnemyLevel.ELevel.level1 && detectC == true || detectTarget == visibleT)         // eLv.LvStep =>enemy.(탐지단계 변수) 으로 바꿔야함
+                        if (enemy.dLevel == (int)eLv.LvStep && detectC == true)
                         {
                             eLv.LvStep = EnemyLevel.ELevel.level2;
-                            detectC = false;
                         }
-                        if (eLv.LvStep == EnemyLevel.ELevel.level2 && detectC == true || detectTarget == visibleT)
+                        if (enemy.dLevel == (int)eLv.LvStep && detectC == true)
                         {
                             eLv.LvStep = EnemyLevel.ELevel.level3;
-                            detectC = false;
                         }
                     }
                     if (this.name == "CCTV")    // CCTV가 플레이어 감지 후 즉시 탐지단계 상승
