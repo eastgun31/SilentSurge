9927aabd495680c67300a008ba95e8bcc91942c1
lcal0824 <kdh9438@naver.com>
2024-04-11 12:53:33 +0900

    문 상호작용 적용 끝
    오브젝트 상호작용은 좀만 더 공부해보고 일단 캐비닛으로 넘어가겠습니다.

--- Assets/KDH/Scripts/InteractiveObj.cs
+++ Assets/KDH/Scripts/InteractiveObj.cs
@@ -4,20 +4,41 @@ using UnityEngine;
 
 public class InteractiveObj : MonoBehaviour     // 상호작용 가능한 오브젝트에 인접했을 때 해당 오브젝트에 아웃라인을 그려주는 스크립트
 {
-    [SerializeField]
-    private Material orgMat;
-    [SerializeField]
-    private Material outlineMat;
+    public Material[] pc_mat;
+    public Material outline_mat;
 
 
 
-    private void OnTriggerStay(Collider other)
-    {
-        this.GetComponent<MeshRenderer>().material = outlineMat;
-    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        this.GetComponent<MeshRenderer>().material = orgMat;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //----------------------------------------------------------------------------------------------
+
+    //[SerializeField]
+    //private Material orgMat;
+    //[SerializeField]
+    //private Material outlineMat;
+
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    this.GetComponentInChildren<MeshRenderer>().material = outlineMat;
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    this.GetComponentInChildren<MeshRenderer>().material = orgMat;
+    //}
 }
