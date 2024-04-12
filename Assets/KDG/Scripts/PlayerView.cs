0b7ba6dbfff0290badc91c15bed088936be0b96f
eastgun31 <133588807+eastgun31@users.noreply.github.com>
2024-04-13 01:22:30 +0900

    김동건_시야수정

--- Assets/KDG/Scripts/PlayerView.cs
+++ Assets/KDG/Scripts/PlayerView.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class PlayerView : MonoBehaviour
 {
     public float viewRadius;
+    [SerializeField]
+    private float viewRadius2;
     [Range(0, 360)]
     public float viewAngle;
+    [SerializeField]
+    private float viewAngle2;
     public LayerMask targetMask, obstacleMask;  //레이어
-    public List<Transform> visibleTargets = new List<Transform>(); // Target mask에 ray hit된 transform을 보관하는 리스트
+    //public List<Transform> visibleTargets = new List<Transform>(); 
     //public Transform target;
     WaitForSeconds delay;
 
     public float meshResolution;
     Mesh viewMesh;
+    Mesh viewMesh2;
     public MeshFilter viewMeshFilter;
+    public MeshFilter viewMeshFilter2;
     public int edgeResolveIterations;
     public float edgeDstThreshold;
     public float maskCutawayDst;
@@ -46,26 +52,33 @@ public class PlayerView : MonoBehaviour
 
     void Start()
     {
+        viewRadius2 = 1f;
+        viewAngle2 = 360f;
         maskCutawayDst = 0.2f;
-        delay = new WaitForSeconds(0.2f);
-        // 0.2초 간격으로 코루틴 호출
-        //StartCoroutine(FindTargetsWithDelay());
+        //delay = new WaitForSeconds(0.1f);
+        //// 0.2초 간격으로 코루틴 호출
+        //StartCoroutine(DrawFieldOfDelay());
         viewMesh = new Mesh();
         viewMesh.name = "View Mesh";
         viewMeshFilter.mesh = viewMesh;
 
+        viewMesh2 = new Mesh();
+        viewMesh2.name = "View Mesh2";
+        viewMeshFilter2.mesh = viewMesh2;
     }
     void LateUpdate()
     {
         DrawFieldOfView();
+        DrawFieldOfView2();
     }
 
-    //IEnumerator FindTargetsWithDelay()
+    //IEnumerator DrawFieldOfDelay()
     //{
     //    while (true)
     //    {
     //        yield return delay;
-    //        FindVisibleTargets();
+    //        DrawFieldOfView();
+    //        DrawFieldOfView2();
     //    }
     //}
 
@@ -104,7 +117,7 @@ public class PlayerView : MonoBehaviour
         for (int i = 0; i <= stepCount; i++)
         {
             float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
-            ViewCastInfo newViewCast = ViewCast(angle);
+            ViewCastInfo newViewCast = ViewCast(angle, viewRadius);
 
             // i가 0이면 prevViewCast에 아무 값이 없어 정점 보간을 할 수 없으므로 건너뛴다.
             if (i != 0)
@@ -114,7 +127,7 @@ public class PlayerView : MonoBehaviour
                 // 둘 중 한 raycast가 장애물을 만나지 않았거나 두 raycast가 서로 다른 장애물에 hit 된 것이라면(edgeDstThresholdExceed 여부로 계산)
                 if (prevViewCast.hit != newViewCast.hit || (prevViewCast.hit && newViewCast.hit && edgeDstThresholdExceed))
                 {
-                    Edge e = FindEdge(prevViewCast, newViewCast);
+                    Edge e = FindEdge(prevViewCast, newViewCast, viewRadius);
 
                     // zero가 아닌 정점을 추가함
                     if (e.PointA != Vector3.zero)
@@ -154,21 +167,81 @@ public class PlayerView : MonoBehaviour
         viewMesh.RecalculateNormals();
     }
 
-    ViewCastInfo ViewCast(float globalAngle)
+    void DrawFieldOfView2()
+    {
+        int stepCount = Mathf.RoundToInt(viewAngle2 * meshResolution);
+        float stepAngleSize = viewAngle2 / stepCount;
+        List<Vector3> viewPoints = new List<Vector3>();
+        ViewCastInfo prevViewCast = new ViewCastInfo();
+
+        for (int i = 0; i <= stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - viewAngle2 / 2 + stepAngleSize * i;
+            ViewCastInfo newViewCast = ViewCast(angle, viewRadius2);
+
+            // i가 0이면 prevViewCast에 아무 값이 없어 정점 보간을 할 수 없으므로 건너뛴다.
+            if (i != 0)
+            {
+                bool edgeDstThresholdExceed = Mathf.Abs(prevViewCast.dst - newViewCast.dst) > edgeDstThreshold;
+
+                // 둘 중 한 raycast가 장애물을 만나지 않았거나 두 raycast가 서로 다른 장애물에 hit 된 것이라면(edgeDstThresholdExceed 여부로 계산)
+                if (prevViewCast.hit != newViewCast.hit || (prevViewCast.hit && newViewCast.hit && edgeDstThresholdExceed))
+                {
+                    Edge e = FindEdge(prevViewCast, newViewCast, viewRadius2);
+
+                    // zero가 아닌 정점을 추가함
+                    if (e.PointA != Vector3.zero)
+                    {
+                        viewPoints.Add(e.PointA);
+                    }
+
+                    if (e.PointB != Vector3.zero)
+                    {
+                        viewPoints.Add(e.PointB);
+                    }
+                }
+            }
+
+            viewPoints.Add(newViewCast.point);
+            prevViewCast = newViewCast;
+        }
+
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+        vertices[0] = Vector3.zero;
+
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]) + Vector3.forward * maskCutawayDst;
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+        viewMesh2.Clear();
+        viewMesh2.vertices = vertices;
+        viewMesh2.triangles = triangles;
+        viewMesh2.RecalculateNormals();
+    }
+
+    ViewCastInfo ViewCast(float globalAngle, float radius)
     {
         Vector3 dir = DirFromAngle(globalAngle, true);
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, dir, out hit, viewRadius, obstacleMask))
+        if (Physics.Raycast(transform.position, dir, out hit, radius, obstacleMask))
         {
             return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
         }
         else
         {
-            return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, globalAngle);
+            return new ViewCastInfo(false, transform.position + dir * radius, radius, globalAngle);
         }
     }
 
-    Edge FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast)
+    Edge FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast, float radius)
     {
         float minAngle = minViewCast.angle;
         float maxAngle = maxViewCast.angle;
@@ -178,7 +251,7 @@ public class PlayerView : MonoBehaviour
         for (int i = 0; i < edgeResolveIterations; i++)
         {
             float angle = minAngle + (maxAngle - minAngle) / 2;
-            ViewCastInfo newViewCast = ViewCast(angle);
+            ViewCastInfo newViewCast = ViewCast(angle, radius);
             bool edgeDstThresholdExceed = Mathf.Abs(minViewCast.dst - newViewCast.dst) > edgeDstThreshold;
             if (newViewCast.hit == minViewCast.hit && !edgeDstThresholdExceed)
             {
