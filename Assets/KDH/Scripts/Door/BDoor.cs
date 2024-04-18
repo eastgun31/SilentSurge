60b5a460499d706e23d3f68934c6cf36574dd835
lcal0824 <kdh9438@naver.com>
2024-04-18 23:57:29 +0900

    보스 문 열리는 함수 구현
    나중에 퍼즐 깨거나, 상호작용 하면 열리게 쓰면 됨

--- Assets/KDH/Scripts/Door/BDoor.cs
+++ Assets/KDH/Scripts/Door/BDoor.cs
@@ -6,4 +6,34 @@ public class BDoor : MonoBehaviour
 {
     public GameObject L_Bdoor;
     public GameObject R_Bdoor;
+
+    public bool bD_Unlock;
+
+    WaitForSeconds bD_openT;
+
+    private void Start()
+    {
+        bD_openT = new WaitForSeconds(1.4f);
+    }
+
+    private void Update()
+    {
+        BDoorOpen();
+    }
+
+    void BDoorOpen()
+    {
+        if(bD_Unlock)
+        {
+            StartCoroutine(Bdoor_opening());
+        }
+    }
+
+    IEnumerator Bdoor_opening()
+    {
+        L_Bdoor.transform.Translate(0.02f, 0, 0);
+        R_Bdoor.transform.Translate(-0.02f, 0, 0);
+        yield return bD_openT;
+        bD_Unlock = false;
+    }
 }
