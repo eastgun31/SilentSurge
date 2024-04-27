ad798de56b3b66e9745f3690fc1b0ba50692ccfd
eastgun31 <133588807+eastgun31@users.noreply.github.com>
2024-04-27 11:00:17 +0900

    김동건_라이트베이크 및 엔딩조건 구현중

--- Assets/KDG/Scripts/GameClear.cs
+++ Assets/KDG/Scripts/GameClear.cs
@@ -2,12 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Linq;
+using ItemInfo;
 
 public class GameClear : MonoBehaviour
 {
     public GameObject clear;
     public int value;
     public UnityEvent lastAction;
+
+    public List<GameObject> enemys;
+    CoolTime cool;
+
+    private void Start()
+    {
+        cool = new CoolTime();
+    }
+
+    IEnumerator EndingCheck()
+    {
+        while (true)
+        {
+            foreach (GameObject enemy in enemys)
+            {
+                if (!enemy.activeSelf)
+                    enemys.Remove(enemy);
+            }
+            
+
+            yield return cool.cool1sec;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
