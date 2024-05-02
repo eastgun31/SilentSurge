b8e611c6c06580a7d69bcc883d36513ca55704d8
lcal0824 <kdh9438@naver.com>
2024-05-02 11:45:34 +0900

    김동현_가이드라인 수정중

--- Assets/KDH/GuideLine/GuideLineTxt.cs
+++ Assets/KDH/GuideLine/GuideLineTxt.cs
@@ -24,8 +24,44 @@ public class GuideLineTxt : MonoBehaviour
     [SerializeField]
     private GuideLineDB guideLineDB;
 
-    private void Start()
+    [SerializeField]
+    private GuidetxtData[] guidetxtDatas;
+    [SerializeField]
+    private GuideTxt[] guideTxts;
+
+    private int currentguidetxtDatasIndex;
+    private int currentguideTxtsIndex;
+
+
+    //void Awake()
+    //{
+    //    int index = 0;
+    //    for (int i = 0; i < guideLineDB.guideLine.Count; ++i)
+    //    {
+    //        if (guideLineDB.guideLine[i].eventNum == eventNum)
+    //        {
+
+    //        }
+    //    }
+
+    //    GuideTxtSetting();
+    //}
+
+    void Update()
     {
         
+    }
+
+    public void GuideTxtSetting()
+    {
+        for(int i = 0; i<guideTxts.Length; ++i)
+        {
+            SetActiveTxt(guideTxts[i],false);
+        }
+    }
+
+    public void SetActiveTxt(GuideTxt guidetxt, bool visible)
+    {
+        guidetxt.guideTxtText.gameObject.SetActive(visible);
     }
 }
