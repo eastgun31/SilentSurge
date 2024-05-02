712cb10be158074a1c498ca37c75a86a47e6806e
kaffly <kgh010105@naver.com>
2024-05-02 22:31:51 +0900

    김건호_0502
    로딩창 자연스럽게 자막 위치설정

--- Assets/KGH/Scripts/UI/LoadingBar.cs
+++ Assets/KGH/Scripts/UI/LoadingBar.cs
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 public class LoadingBar : MonoBehaviour
 {
     public Slider slider;
-    public float time = 5f;
+    public float time = 2f;
 
     private float target;
-    private float loadingtime;
+    private float currentValue;
 
     public GameObject loading;
+
+    public Text loadText;
 
     void Start()
     {
@@ -20,12 +22,24 @@ public class LoadingBar : MonoBehaviour
 
     void Update()
     {
-        loadingtime += Time.deltaTime;
-        float currentvalue = Mathf.Lerp(0f, target, loadingtime / time);
-        slider.value = currentvalue;
-        if(slider.value >= target)
+        currentValue = slider.value;
+        if (currentValue <= slider.maxValue * 0.3f)
+            time = 10f;
+        else if (currentValue <= slider.maxValue * 0.5f)
+            time = 5f;
+        else if (currentValue >= slider.maxValue * 0.9f)
+            time = 15f;
+
+        if (currentValue < target)
         {
-            loading.SetActive(false);
+            currentValue += Time.deltaTime / time;
+            slider.value = currentValue;
         }
+
+        if (slider.value >= target)
+            loading.SetActive(false);
+
+        int percent = Mathf.RoundToInt((currentValue / target) * 100f);
+        loadText.text = $"{percent}%";
     }
 }
