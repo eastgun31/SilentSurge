210189233612018ef7847cab633d1ba0b5f1e01e
lcal0824 <kdh9438@naver.com>
2024-05-02 12:52:41 +0900

    김동현_가이드라인받아오기중

--- Assets/KDH/GuideLine/GuideLineTxt.cs
+++ Assets/KDH/GuideLine/GuideLineTxt.cs
@@ -5,47 +5,50 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [System.Serializable]
-public struct GuidetxtData
+public struct GuideData
 {
-    public int guideIndex;
-    public string guideTxt;
+    public int guideText_Index;
+    public string guideT;
 }
 
 [System.Serializable]
-public struct GuideTxt
+public struct GuideText
 {
-    public Text guideTxtText;
+    public Text guideText;                            // ÀÚ¸·
 }
+
 
 public class GuideLineTxt : MonoBehaviour
 {
     [SerializeField]
-    private int eventNum;
+    private int eventNum;                              //
     [SerializeField]
-    private GuideLineDB guideLineDB;
+    private GuideLineDB guideLineDB;          //
+
 
     [SerializeField]
-    private GuidetxtData[] guidetxtDatas;
+    private GuideData[] guideDatas;               //
     [SerializeField]
-    private GuideTxt[] guideTxts;
-
-    private int currentguidetxtDatasIndex;
-    private int currentguideTxtsIndex;
+    private GuideText[] guideTexts;                 // 
 
 
-    //void Awake()
-    //{
-    //    int index = 0;
-    //    for (int i = 0; i < guideLineDB.guideLine.Count; ++i)
-    //    {
-    //        if (guideLineDB.guideLine[i].eventNum == eventNum)
-    //        {
+    private int currentDatas_Index = -1;           //
+    private int currentTexts_Index = 0;             //
 
-    //        }
-    //    }
 
-    //    GuideTxtSetting();
-    //}
+    void Awake()
+    {
+        int index = 0;
+        for (int i = 0; i < guideLineDB.guideLine.Count; ++i)
+        {
+            if (guideLineDB.guideLine[i].eventNum == eventNum)
+            {
+                guideDatas[index].guideT = guideLineDB.guideLine[i].guideTxt;
+                index++;
+            }
+        }
+        GuideTxtSetting();
+    }
 
     void Update()
     {
@@ -54,14 +57,26 @@ public class GuideLineTxt : MonoBehaviour
 
     public void GuideTxtSetting()
     {
-        for(int i = 0; i<guideTxts.Length; ++i)
+        for(int i = 0; i<guideTexts.Length; ++i)
         {
-            SetActiveTxt(guideTxts[i],false);
+            SetActiveTxt(guideTexts[i],false);
         }
     }
 
-    public void SetActiveTxt(GuideTxt guidetxt, bool visible)
+    public void SetActiveTxt(GuideText guideT, bool visible)
     {
-        guidetxt.guideTxtText.gameObject.SetActive(visible);
+        guideT.guideText.gameObject.SetActive(visible);
+    }
+
+    public void SetOffTxt()
+    {
+        SetActiveTxt(guideTexts[currentTexts_Index], false);
+    }
+
+    public void SetDifferentTxt()
+    {
+        currentTexts_Index = guideDatas[currentDatas_Index].guideText_Index;
+        SetActiveTxt(guideTexts[currentTexts_Index], true);
+        guideTexts[currentTexts_Index].guideText.text = guideDatas[currentDatas_Index].guideT;
     }
 }
---
+++ Assets/KDH/GuideLine/TestGuide.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGuide : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Z))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.C))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.V))
+        {
+
+        }
+    }
+}
