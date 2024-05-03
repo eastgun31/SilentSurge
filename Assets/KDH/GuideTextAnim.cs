ce1ab55e51bc12343da654cdad1998dffbdb80c5
lcal0824 <kdh9438@naver.com>
2024-05-03 12:56:48 +0900

    김동현_자막 구현

---
+++ Assets/KDH/GuideTextAnim.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class GuideTextAnim : MonoBehaviour
+{
+    //public Text gText;
+    //public void Start()
+    //{
+    //    GetComponent<DOTweenAnimation>();
+    //}
+
+    //public void GuideAnim()
+    //{
+    //    gText.DOText(GuideLineTxt.instance.guideUI.text, 2f).SetDelay(2f);
+    //}
+}
